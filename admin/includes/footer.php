fb09884f5f2ef9027ee20f1d2851e9fc26563b93
scshivam <shivam.bkarogyam@gmail.com>
2020-12-16 17:18:43 +0530

    second_commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>@<a href="#">Shourya Gupta</a></b>
+    </div>
+    <strong>Copyright &copy; 2020 Bk Arogyam & Research Pvt. Ltd. </strong>
+</footer>
